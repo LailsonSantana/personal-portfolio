267e7453aa94df5ae291bc3b18dbe8548b7036e5
Lailson Santana <lailsonbit@gmail.com>
2025-04-01 20:28:26 -0300

    Portfolio 1.0

--- src/components/Template.tsx
+++ src/components/Template.tsx
@@ -30,8 +30,12 @@ const Header: React.FC = () => {
 
 const Footer: React.FC = () => {
     return(
-        <>
-        </>
+        <footer className="grid cols-1 md:grid-cols-2 lg: grid-cols-3 gap-8 mb-12 mt-5 bg-[#012030] text-white">
+            <div>
+                <h3 className="text-2xl font-semibold mb-2">Lailson Santana Dev</h3>
+                <p>Texto qualquer</p>
+            </div>
+        </footer>
     )
 }
 
--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -1,9 +1,13 @@
 'use-client'
 
-import { WhatsappLogo } from "@phosphor-icons/react";
 import SectionTitle from "../SectionTitle";
 import useEmblaCarousel from "embla-carousel-react"
-import {Calendar, ChevronLeft, ChevronRight} from "lucide-react"
+import {Calendar, ChevronLeft, ChevronRight,} from "lucide-react"
+import GitHubIcon from "@mui/icons-material/GitHub";
+import AccountTreeIcon from "@mui/icons-material/AccountTree";
+import WebIcon from '@mui/icons-material/Web';
+import QuizIcon from '@mui/icons-material/Quiz';
+import RestaurantIcon from '@mui/icons-material/Restaurant';
 
 const items = [
 
@@ -11,19 +15,29 @@ const items = [
         project : "Portifólio Pessoal",
         description : "Uma landing page para inclusão de informações profissionais , um portifílio online",
         link: "",
+        icon: <WebIcon fontSize="large" className="text-white" />,
         finishedDate: "Em andamento"
     },
     {
         project: "Question Hub",
         description: "Uma plataforma educativa online para criação de questões, criada como trabalho de conclusão de curso(TCC)",
-        link: "",
+        link: "https://github.com/LailsonSantana/question_hub",
+        icon: <QuizIcon fontSize="large" className="text-white" />,
         finishedDate: "20/04/2025"
     },
     {
         project : "Fluxograma Interativo",
         description : "Um fluxograma interativo para verificação de disciplinas e pré requisitos",
-        link: "",
+        link: "https://github.com/LailsonSantana/interactive-flowchart",
+        icon: <AccountTreeIcon fontSize="large" className="text-white" />,
         finishedDate: "01/03/2025"
+    },
+    {
+        project : "Jantar dos Filósofos",
+        description : "Execução visual do problema clássico de programação concorrente chamado jantar dos filósofos",
+        link: "https://github.com/LailsonSantana/Programacao_Concorrente/tree/main/pc_trabalho05_201911924",
+        icon: <RestaurantIcon fontSize="large" className="text-white" />,
+        finishedDate: "23/05/2021"
     },
     
 ]
@@ -51,8 +65,9 @@ const Project: React.FC<ProjectProps> = () => {
         emblaApi?.scrollNext();
     }
     return(
-        <section className="bg-[#45C4B0] p-4">
+        <section className="bg-[#45C4B0] p-12">
             <SectionTitle section="Projetos"/>
+            <div className="relative">
             <div className="overflow-hidden mt-6" ref={emblaRef}>
                 <div className="flex">
                     {items.map((item, index) => (
@@ -60,9 +75,12 @@ const Project: React.FC<ProjectProps> = () => {
                             <article className="bg-[#1e293b] text-white rounded-2xl p-6 space-y-4 h-full flex flex-col">
                                 <div className="flex-1 flex itms-start justify-between">
                                     <div className="flex gap-3">
-                                        <span className="text-3xl"></span>
+                                        <span className="text-3xl">
+                                            {item.icon}
+                                        </span>
                                         <div>
-                                            <h3 className="font-bold text-xl my-1" >
+                                            
+                                            <h3 className="font-bold text-xl my-1 mb-4" >
                                                 {item.project}
                                             </h3>
                                             <p className="text-gray-400 select-none">{item.description}</p>
@@ -75,9 +93,9 @@ const Project: React.FC<ProjectProps> = () => {
                                         <span>Finalizado em: {item.finishedDate}</span>
                                     </div>
 
-                                    <a href="#" className="flex items-center justify-center px-4 py-1 rounded-md">
-                                        <WhatsappLogo className="w-5 h-5"/>
-                                        Ver mais
+                                    <a href={item.link} className="flex items-center justify-center px-4 py-1 rounded-md">
+                                        <GitHubIcon fontSize="small" />
+                                        <span className="ml-2">Visualizar</span>
                                     </a>
                                 </div>
                             </article>
@@ -85,13 +103,21 @@ const Project: React.FC<ProjectProps> = () => {
                     ))}
                 </div>
             </div>
-
-            <button className="bg-white flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute left-3 -translate-y-1/2 -translate-x-1/2 top-1/3" 
+            
+            <button className='bg-white flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute left-3 -translate-y-1/2 -translate-x-1/2 top-1/2 z-10' 
                     onClick={scrollPrev}>
                 <ChevronLeft 
                 className="w-6 h-6 text-gray-600 " />
                 
             </button>
+
+            <button className='bg-white flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute -right-6 -translate-y-1/2 -translate-x-1/2 top-1/2 z-10' 
+                    onClick={scrollNext}>   
+                <ChevronRight 
+                className="w-6 h-6 text-gray-600 " />
+                
+            </button>
+            </div>
 
 
         </section>
